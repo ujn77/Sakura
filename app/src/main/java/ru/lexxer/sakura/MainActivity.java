0b7f65d8863b5762433c70b166c9aa4e602c9159
Alexey Buynichev <lex1977@gmail.com>
2016-06-30 22:45:27 +0600

    Revert "try to add MaterialDrawer menu"
    This reverts commit 9554fa43909c7328698885c82288faa1315dcf91.

--- app/src/main/java/ru/lexxer/sakura/MainActivity.java
+++ app/src/main/java/ru/lexxer/sakura/MainActivity.java
@@ -2,20 +2,10 @@ package ru.lexxer.sakura;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
-import android.widget.FrameLayout;
 import android.widget.ListView;
 import android.widget.Toast;
-
-import com.mikepenz.materialdrawer.AccountHeader;
-import com.mikepenz.materialdrawer.AccountHeaderBuilder;
-import com.mikepenz.materialdrawer.Drawer;
-import com.mikepenz.materialdrawer.DrawerBuilder;
-import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
-import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
-import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 
 import java.io.IOException;
 
@@ -27,16 +17,6 @@ import ru.lexxer.sakura.network.RetrofitService;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Drawer drawer = null;
-    private Toolbar toolbar;
-
-    private String[] channelNames;
-    private String[] channelId;
-    private String[] videoTypes;
-
-    private int selectedDrawerItem = 0;
-
-    private FrameLayout layoutList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,54 +28,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         buttonGetGoods.setOnClickListener(this);
 
-        layoutList = (FrameLayout) findViewById(R.id.fragment_container);
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
         //ProductAdapter mProductAdapter = new ProductAdapter(this);
-
-        channelNames = getResources().getStringArray(R.array.channel_names);
-        channelId = getResources().getStringArray(R.array.channel_id);
-        videoTypes = getResources().getStringArray(R.array.video_types);
-
-        PrimaryDrawerItem[] primaryDrawerItems = new PrimaryDrawerItem[channelId.length];
-
-        for (int i = 0; i < channelId.length; i++) {
-            primaryDrawerItems[i] = new PrimaryDrawerItem()
-                    .withName(channelNames[i])
-                    .withIdentifier(i)
-                    .withSelectable(false);
-        }
-        AccountHeader accountHeader = new AccountHeaderBuilder()
-                .withActivity(this)
-                .withHeaderBackground(R.drawable.header)
-                .build();
-
-        drawer = new DrawerBuilder(this)
-                .withActivity(MainActivity.this)
-                .withAccountHeader(accountHeader)
-                .withDisplayBelowStatusBar(true)
-                //.withToolbar(toolbar)
-                .withActionBarDrawerToggleAnimated(true)
-                .withSavedInstance(savedInstanceState)
-                .addDrawerItems(primaryDrawerItems)
-                .addStickyDrawerItems(
-                        new SecondaryDrawerItem()
-                                .withName(getString(R.string.about))
-                                .withIdentifier(channelId.length - 1)
-                                .withSelectable(false)
-                )
-                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
-                    @Override
-                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
-                        return false;
-                        //
-                    }
-                })
-                .withSavedInstance(savedInstanceState)
-                .withShowDrawerOnFirstLaunch(true)
-                .build();
-
     }
 
     @Override
